ca6a4bfb08f194d1c40e3cae1a6499aa7e1c0ad0
Rishabh Raj <rishabhraj777@gmail.com>
2025-07-23 22:30:13 +0530

    Added deploy, service & updated codes

--- app/app.js
+++ app/app.js
@@ -5,23 +5,24 @@ const todoRoutes = require('./routes/todoRoutes');
 const path = require('path');
 
 dotenv.config();
-const app = express();
 
+const app = express();
 const mongoURI = process.env.MONGO_URI;
+
 console.log('Connecting to MongoDB at:', mongoURI);
 
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-// Routes
 app.use('/', todoRoutes);
 
-// Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
